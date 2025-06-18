f7d1c5b6d1055c8ddd804de2dd0d56a6a5d6f680
mahfuzahmmedabir <mahfuz.abir06@gmail.com>
2025-06-18 08:25:45 +0100

    calculate time complexity

---
+++ Time and space complexity/found.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // int n, q;
+    // cin >> n >> q;
+    // int arr[n];
+    // for(int i = 0; i<n; i++){
+    //    cin >> arr[i];
+    // }
+    // for(int i = 0; i<q; i++){
+        
+    //     int x; 
+    //     cin >> x;
+    //     int falge = 0;
+
+    //     for(int i = 0; i < n; i++){
+    //         if(arr[i] == x){
+    //             falge = 1;
+    //         }
+    //     }
+
+    //     if(falge == 1 ){
+    //         cout << "found" <<endl;
+    //     }else{
+    //         cout << "Not found" << endl;
+    //     }
+    // }
+
+    int n;
+    cin>>n;
+    for(int i = 0; i < n /2;i++){
+    cout << i << endl;
+    }
+    return 0; 
+}
--- Time and space complexity/time_complexity.cpp
+++ Time and space complexity/time_complexity.cpp
@@ -1,6 +1,17 @@
  #include<bits/stdc++.h>
  using namespace std;
  int main(){
+
+    int n;
+    cin >> n;
+    int sum = 0;
+    // for(int i = 0; i <= n; i++){
+    //     sum += i;
+    // }
+
+    sum = (n* (n+1))/2;
+
+    cout << sum;
      
      return 0; 
  }
